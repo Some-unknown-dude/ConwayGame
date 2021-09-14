6bb29f2a6b68b30cf9904b060540b147e0486005
Vladislav Kochegin <kocheginvladislav@gmail.com>
2021-09-14 04:09:54 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,36 @@
+﻿using Logic;
+using System;
+using System.Threading;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random((int)DateTime.Now.Ticks);
+            Grid grid = new Grid(GetRandomBool2DArray(random));
+            while (true)
+            {
+                Console.WriteLine(grid);
+                grid.Update();
+                Thread.Sleep(100);
+                Console.SetCursorPosition(0, 0);
+            }
+        }
+
+        public static bool[,] GetRandomBool2DArray(Random random)
+        {
+            bool[,] result = new bool[Grid.size, Grid.size];
+            for (int i = 0; i < Grid.size; ++i)
+            {
+                for (int j = 0; j < Grid.size; ++j)
+                {
+                    result[i, j] = random.Next(0, 2) == 0 ? false : true;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Logic/Grid.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logic
+{
+    public class Grid
+    {
+        public const int size = 100;
+        private bool[,] grid = new bool[size, size];
+
+        public Grid(bool[,] array)
+        {
+            this.grid = array;
+        }
+
+        public void Update()
+        {
+            bool[,] newGrid = (bool[,])grid.Clone();
+            for (int i = 0; i < size; ++i)
+            {
+                for (int j = 0; j < size; ++j)
+                {
+                    newGrid[i, j] = this.UpdateToANewState(this.grid[i, j], GetAllNeighboutCells(i, j));
+                }
+            }
+
+            this.grid = newGrid;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < size; ++i)
+            {
+                for (int j = 0; j < size; ++j)
+                {
+                    result.Append(this.grid[i, j] ? '█' : ' ');
+                }
+
+                result.Append('\n');
+            }
+
+            return result.ToString();
+        }
+
+        private void ValidateCoordinates((int row, int column)[] startCoordinates)
+        {
+            foreach (var (row, column) in startCoordinates)
+            {
+                if (row < 0 || row >= size)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(row), $"{nameof(row)} must be between 0 and ({nameof(size)} - 1) inclusive.");
+                }
+
+                if (column < 0 || column >= size)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(row), $"{nameof(row)} must be between 0 and ({nameof(size)} - 1) inclusive.");
+                }
+            }
+        }
+
+        private bool UpdateToANewState(bool initialState, IEnumerable<bool> neighbours)
+        {
+            int aliveNeighboursCount = neighbours.Count(c => c);
+            if (initialState)
+            {
+                return (aliveNeighboursCount == 2 || aliveNeighboursCount == 3);
+            }
+
+            return aliveNeighboursCount == 3;
+        }
+
+        private IEnumerable<bool> GetAllNeighboutCells(int row, int column)
+        {
+            for (int i = -1; i <= 1; ++i)
+            {
+                if (IsInRange(row + i, column - 1))
+                {
+                    yield return this.grid[row + i, column - 1];
+                }
+
+                if (IsInRange(row + i, column + 1))
+                {
+                    yield return this.grid[row + i, column + 1];
+                }
+            }
+
+            if (IsInRange(row - 1, column))
+            {
+                yield return this.grid[row - 1, column];
+            }
+
+            if (IsInRange(row + 1, column))
+            {
+                yield return this.grid[row + 1, column];
+            }
+        }
+
+        private bool IsInRange(int row, int column) => row >= 0 && row < size && column >= 0 && column < size;
+    }
+}
